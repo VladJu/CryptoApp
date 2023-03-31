bc527fd4089065aeae0fcb5350279f9f8f6c5f56
VladJu <juikovvlad@gmail.com>
2023-03-31 14:14:13 +0300

    Use viewBinding in adapter

--- app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
@@ -2,16 +2,15 @@ package com.example.cryptoapp.presentation.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptoapp.R
+import com.example.cryptoapp.databinding.ItemCoinInfoBinding
 import com.example.cryptoapp.domain.CoinInfoEntity
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.item_coin_info.view.*
 
-class CoinInfoAdapter(private val context: Context) :
-    RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
+
+class CoinInfoAdapter(private val context: Context) : RecyclerView.Adapter<CoinInfoViewHolder>() {
 
     var coinInfoList: List<CoinInfoEntity> = listOf()
         //когда переменной будем присваивать новое значение мы хотим вызывать метод notifyDataSetChanged(),
@@ -21,24 +20,23 @@ class CoinInfoAdapter(private val context: Context) :
             notifyDataSetChanged()
         }
 
-    // 2 переменная котрой можно присвоить значение из активити
+
     var onCoinClickListener: OnCoinClickListener? = null
 
-    //Создаем view из макета
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_coin_info,
+        val binding = ItemCoinInfoBinding.inflate(
+            LayoutInflater.from(parent.context),
             parent,
             false
         )
-        return CoinInfoViewHolder(view)
+        return CoinInfoViewHolder(binding)
     }
 
-    //по номеру позиции показвыает какой элемент нам надо сейчас отобразить( и какой текст,цвет там установить)
+
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
         val coin = coinInfoList[position]
-        //устанавилваем значения нужным нам элеентам
-        with(holder) {
+        with(holder.binding) {
             with(coin) {
                 val symbolsTemplate = context.resources.getString(R.string.symbols_template)
                 val lastUpdateTemplate = context.resources.getString(R.string.last_update_template)
@@ -48,8 +46,7 @@ class CoinInfoAdapter(private val context: Context) :
                     lastUpdateTemplate, lastUpdate
                 )
                 Picasso.get().load(imageUrl).into(ivLogoCoin)
-                // 3 при клике на itemView будет првоерено занчение !=0 .onCoinClick(coin)
-                itemView.setOnClickListener {
+                root.setOnClickListener {
                     onCoinClickListener?.onCoinClick(coin)
                 }
             }
@@ -61,15 +58,6 @@ class CoinInfoAdapter(private val context: Context) :
         return coinInfoList.size
     }
 
-    inner class CoinInfoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val ivLogoCoin = itemView.ivLogoCoin
-        val tvSymbols = itemView.tvSymbols
-        val tvPrice = itemView.tvPrice
-        val tvLastTimeUpdate = itemView.tvLastTimeUpdate
-
-    }
-
-    // 1
     interface OnCoinClickListener {
         fun onCoinClick(coinPriceInfo: CoinInfoEntity)
     }
